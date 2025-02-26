3493b2b97dda1d7df9ab5643026e106bb3cf3047
ryanrnjit <rjr@njit.edu>
2025-02-25 23:21:08 -0500

    Revert "Bootstrap setup"
    This reverts commit 1ab68cad67d1cd519248ea2668c2d6095d9eb683.

--- films/src/App.js
+++ films/src/App.js
@@ -12,8 +12,6 @@ import ActorDetails from './pages/actordetails.js';
 import Films from './pages/films.js'
 import Customers from './pages/customers.js';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 function App() {
     return(
         <Router>
